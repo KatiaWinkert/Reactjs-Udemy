fce1d0d63ab9e633dad7f37c72c601cc01298586
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-27 00:02:31 -0300

    App.js
    react aquivo

---
+++ 1_intro/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world React</h1>
+      <p>Aprendendo a cada dia</p>
+    </div>
+  )
+}
+
+export default App
