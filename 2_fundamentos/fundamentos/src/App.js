c0f48f4fcc1af52ec3794520d907fe04d4f6f59d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-27 20:35:45 -0300

    Commit
    Atualização

---
+++ 2_fundamentos/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// Components //
+import FristComponent from './components/FirstComponent'
+
+//styles css //
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FristComponent />
+    </div>
+  )
+}
+
+export default App
