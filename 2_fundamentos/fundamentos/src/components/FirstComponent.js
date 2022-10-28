1278201909228c72828a09ba08c4ec74e3e87051
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-27 21:26:19 -0300

    Firstcomponents
    Incluindo comentario no jsx

--- 2_fundamentos/fundamentos/src/components/FirstComponent.js
+++ 2_fundamentos/fundamentos/src/components/FirstComponent.js
@@ -1,7 +1,11 @@
+import MyComponent from "./MyComponent"
+
 const FristComponent = () => {
   return (
     <div>
+      {/* comentario */ }
       <h1>Meu primeiro componente</h1>
+      <MyComponent/>
     </div>
   )
 }
