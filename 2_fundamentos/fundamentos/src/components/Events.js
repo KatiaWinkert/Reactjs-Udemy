dfa04a43f36963f2c61df7201453bc90b7cf46a5
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 00:19:11 -0300

    commit
    Atualização

--- 2_fundamentos/fundamentos/src/components/Events.js
+++ 2_fundamentos/fundamentos/src/components/Events.js
@@ -1,24 +1,37 @@
 const Events = () => {
   const handleMyEvent = (e) => {
     console.log(e)
+    console.log('Ativou o evento!')
   }
-
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso!</h1>
+    } else {
+      return <h1>Também posso renderizar isso!</h1>
+    }
+  }
+  
   return (
     <div>
       <div>
         <button onclick={handleMyEvent}>Clique aqui!</button>
       </div>
       <div>
-        <button onclick={()=> console.log('Clicou!')}>Clique Aqui! 
-        </button>
+        <button onclick={() => console.log('Clicou!')}>Clique Aqui!</button>
       </div>
       <div>
-        <button onclick={() => {
-            if (true){
-                console.log("isso não deveria existir =)")
+        <button
+          onclick={() => {
+            if (true) {
+              console.log('isso não deveria existir =)')
             }
-        }}>Clica aqui por favor!</button>
+          }}
+        >
+          Clica aqui por favor!
+        </button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   )
 }
