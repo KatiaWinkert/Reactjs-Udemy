42c349c0639efbf369395564b8c17d2b777cb818
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 00:32:53 -0300

    Challenge
    Atividade incluindo event onclick para soma

--- 2_fundamentos/fundamentos/src/components/ChallEnge.js
+++ 2_fundamentos/fundamentos/src/components/ChallEnge.js
@@ -1,13 +1,17 @@
 const ChallEnge = () => {
-
-    const a = 2 
-    const b = 2
-    return(
-        <div>
-            <p> Qual valor da soma: {a+b}</p>
-        </div>
-    )
-
+  const a = 20
+  const b = 15
+  return (
+    <div>
+      <p>
+        {' '}
+        A: {a} e b: {b}
+      </p>
+      <button onClick={() => console.log(a + b)}>
+        clique para fazer a soma de A + B
+      </button>
+    </div>
+  )
 }
 
 export default ChallEnge
