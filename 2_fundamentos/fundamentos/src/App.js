fc021caf8d8aeacf4e260d63edd6173fb5c8ac08
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-27 21:25:37 -0300

    App.js
    importação de componentes

--- 2_fundamentos/fundamentos/src/App.js
+++ 2_fundamentos/fundamentos/src/App.js
@@ -1,14 +1,18 @@
 // Components //
 import FristComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
+
 
 //styles css //
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <FristComponent />
+      <TemplateExpressions/>
     </div>
   )
 }
