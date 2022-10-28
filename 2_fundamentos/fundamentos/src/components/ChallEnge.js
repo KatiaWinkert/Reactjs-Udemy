c7a48f5e8f1e334e39766bc30e39a7bd2bdfe982
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 00:20:03 -0300

    ChallEnge.js
    Desafio 2

---
+++ 2_fundamentos/fundamentos/src/components/ChallEnge.js
@@ -0,0 +1,13 @@
+const ChallEnge = () => {
+
+    const a = 2 
+    const b = 2
+    return(
+        <div>
+            <p> Qual valor da soma: {a+b}</p>
+        </div>
+    )
+
+}
+
+export default ChallEnge
