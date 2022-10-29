cb5990b757e120f97071d5172ff2678bd10c25c2
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 23:09:02 -0300

    commit
    Aula sobre If Ternario no React atualização 23:08

--- 3_Avançando_React/desafio-tres/src/components/ConditonalRender.js
+++ 3_Avançando_React/desafio-tres/src/components/ConditonalRender.js
@@ -3,11 +3,25 @@ import { useState } from 'react'
 const ConditonalRender = () => {
   const [x] = useState(false)
 
+  const [name, setName] = useState("kátia")
+
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso</p>}
+      <h1>If ternario</h1>
+      {name === "João" ? (
+        <div>
+            <p>O nome é João.</p>
+        </div>
+      ) : (
+        <div>
+            <p>O nome foi encontrado!</p>
+        </div>
+
+      )}
+      <button onClick={() => setName("João") }>clique aqui</button>
     </div>
   )
 }
