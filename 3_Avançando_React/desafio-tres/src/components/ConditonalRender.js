1e9a718c9aa3b44f01bced723cfa522a106beff2
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 23:10:28 -0300

    commit

--- 3_Avançando_React/desafio-tres/src/components/ConditonalRender.js
+++ 3_Avançando_React/desafio-tres/src/components/ConditonalRender.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const ConditonalRender = () => {
   const [x] = useState(false)
 
-  const [name, setName] = useState("kátia")
+  const [name, setName] = useState('kátia')
 
   return (
     <div>
@@ -11,17 +11,16 @@ const ConditonalRender = () => {
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Agora x é falso</p>}
       <h1>If ternario</h1>
-      {name === "João" ? (
+      {name === 'João' ? (
         <div>
-            <p>O nome é João.</p>
+          <p>O nome é João.</p>
         </div>
       ) : (
         <div>
-            <p>O nome foi encontrado!</p>
+          <p>O nome foi encontrado!</p>
         </div>
-
       )}
-      <button onClick={() => setName("João") }>clique aqui</button>
+      <button onClick={() => setName('João')}>clique aqui</button>
     </div>
   )
 }
