153d4809cf8fca7d672a442380178c3c127eea57
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 22:41:28 -0300

    Commit
    Atualização da aula 22:41

--- 3_Avançando_React/desafio-tres/src/components/ListeRender.js
+++ 3_Avançando_React/desafio-tres/src/components/ListeRender.js
@@ -3,11 +3,20 @@ import { useState } from 'react'
 const ListeRender = () => {
   const [list] = useState(['Katia', 'Ana', 'Cindy'])
 
-  const [users] = useState([
-    {id:543486, name:"Kátia", age: 33},
-    {id:354687, name: "Nino", age: 7},
-    {id:356468, name: "Jheyni", age: 3},
+  const [users, setUsers] = useState([
+    {id:1, name:"Kátia", age: 33},
+    {id:2, name: "Nino", age: 7},
+    {id:3, name: "Jheyni", age: 3},
   ])
+
+  const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4)
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user)=> randomNumber !== user.id)
+        })
+  }
 
 
   return (
@@ -22,6 +31,7 @@ const ListeRender = () => {
             <li key={user.id}>{user.name} - {user.age}</li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
