638d49480cd8d65e6fb33c85c4b47fbc110cb470
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-28 22:22:20 -0300

    ManageData.js
    atualização

--- 3_Avançando_React/desafio-tres/src/components/ManageData.js
+++ 3_Avançando_React/desafio-tres/src/components/ManageData.js
@@ -1,19 +1,19 @@
-import {useState} from 'react';
+import { useState } from 'react'
 
 const ManageData = () => {
-    let someData =10; 
-    const [number, setNumber] = useState(15);
+  let someData = 10
+  const [number, setNumber] = useState(15)
 
   return (
     <div>
-        <div>
-            <p>Valor : {someData}</p>
-            <button onClick= {() => (someData = 15)}>Mudar variavel</button>
-        </div>
-        <div>
-            <p>Valor: {number}</p>
-            <button onClick={()=> setNumber(25)}>Mudar valor State</button>
-        </div>
+      <div>
+        <p>Valor : {someData}</p>
+        <button onClick={() => (someData = 15)}>Mudar variavel</button>
+      </div>
+      <div>
+        <p>Valor: {number}</p>
+        <button onClick={() => setNumber(25)}>Mudar valor State</button>
+      </div>
     </div>
   )
 }
