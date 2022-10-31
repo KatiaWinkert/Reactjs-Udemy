d2101aa7c536d7c1aebeb15d6360896166fe2cab
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-31 01:01:36 -0300

    CarDetails.js
    Aula sobre reaproveitamento

--- 3_Avançando_React/desafio-tres/src/components/CarDetails.js
+++ 3_Avançando_React/desafio-tres/src/components/CarDetails.js
@@ -1,12 +1,13 @@
-const CarDetails = ({brand, km, color}) => {
+const CarDetails = ({ brand, km, color, newCar }) => {
   return (
     <div>
-        <h2>Detalhe do carro</h2>
-        <ul>
-            <li>Marca: {brand}</li>
-            <li>Km: {km}</li>
-            <li>Cor: {color}</li>
-        </ul>
+      <h2>Detalhe do carro</h2>
+      <ul>
+        <li>Marca: {brand}</li>
+        <li>Km: {km}</li>
+        <li>Cor: {color}</li>
+      </ul>
+      {newCar && <p>Este carro é novo</p>}
     </div>
   )
 }
