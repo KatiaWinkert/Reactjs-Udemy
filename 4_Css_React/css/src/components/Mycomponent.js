0a1022dabeffbae55077634d8be999e6c8446f9b
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-31 22:59:48 -0300

    Mycomponent.js
    aula css em componente

---
+++ 4_Css_React/css/src/components/Mycomponent.js
@@ -0,0 +1,14 @@
+import './Mycomponent.css';
+
+const Mycomponent = () => {
+  return (
+    <div>
+        <h1>Css de componente</h1>
+        <p>Esse é o paragrafo do componente</p>
+        <p className="my-comp-paragraph">Este tambem é do componente</p>
+
+    </div>
+  )
+}
+
+export default Mycomponent
