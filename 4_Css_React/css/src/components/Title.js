5955f541aa9fba8d29e5f6d33e8f7812d8666720
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-02 17:30:35 -0300

    Title.js
    aula

---
+++ 4_Css_React/css/src/components/Title.js
@@ -0,0 +1,11 @@
+import styles from './Title.Module.css'
+
+const Title = () => {
+  return (
+    <div>
+      <h1 className={styles.my_title}>Meu titulo</h1>
+    </div>
+  )
+}
+
+export default Title
