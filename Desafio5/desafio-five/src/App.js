5ccf61d7731b51ccfe8f94a175a780e97804af48
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-04 22:53:34 -0300

    App.js
    Atualização

--- Desafio5/desafio-five/src/App.js
+++ Desafio5/desafio-five/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import CarDetalhe from './Components/CarDetalhe';
+import './App.css'
+import CarDetalhe from './Components/CarDetalhe'
 
 function App() {
+  const cars = [
+    { id: 1, brand: 'Toyota', color: 'Grey', km: 0, newCar: true },
+    { id: 2, brand: 'Audi', color: 'Black', km: 14.805, newCar: false },
+    { id: 3, brand: 'Ferrari', color: 'Red', km: 45.866, newCar: false },
+    { id: 1, brand: 'Ford', color: 'White', km: 0, newCar: true },
+  ]
   return (
     <div className="App">
-    <h1>Challengecss</h1>
-    <CarDetalhe/>
+      <h1>Challengecss</h1>
+      {cars.map((car) => (
+        <CarDetalhe key={car.id} 
+        brand={car.brand}
+        color={car.color}
+        km={car.km}
+        newCar={car.newCar}/>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
