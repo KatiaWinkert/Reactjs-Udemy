f3c3653e1446291316f1cef59af1928dba47ee5e
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-04 22:53:55 -0300

    CerDetalhe.js
    Exercicio Desafiio 5

--- Desafio5/desafio-five/src/Components/CarDetalhe.js
+++ Desafio5/desafio-five/src/Components/CarDetalhe.js
@@ -1,8 +1,16 @@
 import styles from './CarDetalhe.module.css';
 
-const CarDetalhe = () => {
+const CarDetalhe = ({brand, km, color, i,newCar }) => {
   return (
-    <div className={styles.car}>CarDetalhe</div>
+    <div className={styles.car}>
+      <h1>Detalhe de carro </h1>
+      <ul>
+        <li key={i}> Marca:{brand}</li>
+        <li> km: {km}</li>
+        <li> Cor: {color}</li>
+      </ul>
+      {newCar && <p>Este carro é novo</p>}
+    </div>
   )
 }
 
