65adb1939c66f8c6a8ee22e97b97280015e04bb3
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-04 21:09:02 -0300

    app.js
    Atualização

---
+++ Desafio5/desafio-five/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CarDetalhe from './Components/CarDetalhe';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Challengecss</h1>
+    <CarDetalhe/>
+    </div>
+  );
+}
+
+export default App;
