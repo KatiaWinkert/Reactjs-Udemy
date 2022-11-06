81a111c4627814ecd68beff734ad780ae0e70cf8
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-06 20:49:38 -0300

    Commit
    Atualização

--- 5_Form_Em_React/form/src/Components/MyForm.js
+++ 5_Form_Em_React/form/src/Components/MyForm.js
@@ -1,28 +1,59 @@
 import './MyForm.css'
-import {useState} from 'react'
+import { useState } from 'react'
 
-const MyForm = () => {
-    //3-gerenciamento de dados 
+const MyForm = ({user}) => { 
+  //6 - controlled inputs
+  //3-gerenciamento de dados
 
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+  const [name, setName] = useState(user ? user.name : "")
+  const [email, setEmail] = useState(user ? user.email : "")
 
-        const handleName = (e) => {
-            setName(e.target.value)
-        }
-console.log(name)
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+ 
+ //console.log(name)
+ //console.log(email)
+
+  const handleSubmit =(event) =>{
+    event.preventDefault()
+    console.log("enviando o formulario")
+    console.log(name, email)
+// 7 - limpar form 
+// nesse passo é feito apos a validação 
+// e o envia
+
+    setName("")
+    setEmail("")
+  }
+
   return (
+    
     <div>
+      {/*5- Envio de Form*/}
       {/*1-Criação de Form - opção 1*/}
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name " placeholder="Digite o seu nome." onChange={handleName} />
+          <input
+            type="text"
+            name="name "
+            placeholder="Digite o seu nome."
+            onChange={handleName}
+            value={name}
+          />
         </div>
         {/*2-Label envolvendo input- opção 2*/}
         <label>
           <span>E-mail</span>
-          <input type="email" name="email" placeholder="Digite seu email." />
+          {/*4 - Simplificação de manupulação de state*/}
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu email."
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </label>
 
         <input type="submit" value="Enviar" />
