100dde06865994d8b5a2af4a4e0543df82ce17ad
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-06 21:28:38 -0300

    Commit
    Atualização 21;28

--- 5_Form_Em_React/form/src/App.js
+++ 5_Form_Em_React/form/src/App.js
@@ -1,14 +1,20 @@
-
-import './App.css';
-import MyForm from './Components/MyForm';
+import './App.css'
+import MyForm from './Components/MyForm'
 
 function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm user={{name:"Josias", email:"josias@gmail.com"}}/>
+      <MyForm
+        user={{
+          name: 'Josias',
+          email: 'josias@gmail.com',
+          bio: 'Sou uma engenheira',
+          role: 'Administrador',
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- 5_Form_Em_React/form/src/Components/MyForm.js
+++ 5_Form_Em_React/form/src/Components/MyForm.js
@@ -1,34 +1,37 @@
 import './MyForm.css'
 import { useState } from 'react'
 
-const MyForm = ({user}) => { 
+const MyForm = ({ user }) => {
   //6 - controlled inputs
   //3-gerenciamento de dados
 
-  const [name, setName] = useState(user ? user.name : "")
-  const [email, setEmail] = useState(user ? user.email : "")
+  const [name, setName] = useState(user ? user.name : '')
+  const [email, setEmail] = useState(user ? user.email : '')
+
+  const [bio, setBio] = useState(user ? user.bio : '')
+  const [role, setRole] = useState(user ? user.role : '')
 
   const handleName = (e) => {
     setName(e.target.value)
   }
- 
- //console.log(name)
- //console.log(email)
 
-  const handleSubmit =(event) =>{
+  //console.log(name)
+  //console.log(email)
+
+  const handleSubmit = (event) => {
     event.preventDefault()
-    console.log("enviando o formulario")
-    console.log(name, email)
-// 7 - limpar form 
-// nesse passo é feito apos a validação 
-// e o envia
+    console.log('enviando o formulario')
+    console.log(name, email, bio, role)
+    // 7 - limpar form
+    // nesse passo é feito apos a validação
+    // e o envia
 
-    setName("")
-    setEmail("")
+    setName('')
+    setEmail('')
+    setBio('')
   }
 
   return (
-    
     <div>
       {/*5- Envio de Form*/}
       {/*1-Criação de Form - opção 1*/}
@@ -55,7 +58,29 @@ const MyForm = ({user}) => {
             value={email}
           />
         </label>
-
+        {/*8 - Textarea*/}
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuario"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        {/*9 - Select input */}
+        <label>
+          <span>Função dos sistema:</span>
+          <select
+            name="role"
+            onChange={(e) => setRole(e.target.value)}
+            value={role}
+          >
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="adm">Administrador</option>
+          </select>
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
