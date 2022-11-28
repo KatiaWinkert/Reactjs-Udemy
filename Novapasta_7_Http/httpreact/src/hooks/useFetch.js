cbb8b0dd5808363dfac17549fdaac2c436c60967
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-11-27 22:11:18 -0300

    useFetch.js
    aula 5 Refatorando o Post

--- Novapasta_7_Http/httpreact/src/hooks/useFetch.js
+++ Novapasta_7_Http/httpreact/src/hooks/useFetch.js
@@ -1,21 +1,55 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 // 4 - custom hook
 
 export const useFetch = (url) => {
-    const [data, setData] = useState (null)
+  const [data, setData] = useState(null)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(url)
+  // 5 Refatorando o POST
+  const [config, setConfig] = useState(null) // configurar o post e cabeçalhos
+  const [method, setMethod] = useState(null) // essa linha diz qaul metodo estarei usando Get ou Post
+  const [callFetch, setCallFetch] = useState(false) // esse codigo serve para entrar
 
-            const json = await res.json()
-
-            setData(json)
-
+    const httpConfig = (data, method) => {
+        if(method === "POST") {
+            setConfig({
+                method,
+                Headers: {
+                    "Content-type": "application/json"
+                }, 
+                body: JSON.stringify(data)
+            })
         }
-        fetchData()
-    }, [url])
 
-    return {data}
+    }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url)
+
+      const json = await res.json()
+
+      setData(json)
+    }
+    fetchData()
+  }, [url, callFetch])
+
+  // 5 refatorando Post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === 'POST') {
+        let fetchOptions = [url, config]
+
+        const res = await fetch(...fetchOptions)
+
+        const json = await res.json()
+
+        setCallFetch(json)
+      }
+    }
+
+    httpRequest()
+  }, [config])
+
+  return { data, httpConfig }
 }
