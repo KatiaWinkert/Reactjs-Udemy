8196d80533b2e7bea902d36ad843dcebda5abecf
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-08 22:57:35 -0300

    App;js
    inclusão do processo Deletar produtos

--- Desafio6_Http/desafiohttp/src/App.js
+++ Desafio6_Http/desafiohttp/src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+
+//4 Custom hook
+import { useFetch } from './hooks/useFetch'
+
+const url = 'http://localhost:3000/products'
 
 function App() {
+  const [products, setProducts] = useState([])
+  //4 custom hook
+  const { data: itens, httpConfig, loading, error } = useFetch(url)
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  // 1- resgatando dados
+  //useEffect(() => {
+  //async function fetchData() {
+  //const res = await fetch(url)
+  //const data = await res.json()
+
+  //setProducts(data)
+  //}
+  //fetchData()
+  //}, [])
+
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+    /*const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    })
+
+    // 3 carregamento dinamico de dados.
+
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])*/
+    //5 - refatorando post
+    httpConfig(product, 'POST')
+
+    setName('')
+    setPrice('') // esses dois comandos exclui limpa a lista
+  }
+
+  // 8 - desafio 6
+  const handleRemove = (id) => {
+    httpConfig(id, 'DELETE')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Desafio Http number 6</h1>
+      {/* 6 loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {itens &&
+          itens.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
+            </li>
+          ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* 7 - estado de loading de Post*/}
+          {loading && <input type="submit" disabled value="Aguarde!" />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
