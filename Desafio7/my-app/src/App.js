c0fb869132586359cc5c5154644cf41a96429590
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-09 23:42:04 -0300

    App.js
    importação de arquivos, e implementação de React Route.

--- Desafio7/my-app/src/App.js
+++ Desafio7/my-app/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg'
 import './App.css'
+
+// 1 - config react route 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// pages 
+import Home from './pages/Home'
+import About from './pages/About'
+
+//components 
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={< Home />} />
+          <Route path="/about" element={< About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
