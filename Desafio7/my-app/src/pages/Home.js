197f8f3281bec71c2bece2c2669e4f2e22ece08c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-12 00:07:05 -0300

    Home.js
    construção da inclusão de item

--- Desafio7/my-app/src/pages/Home.js
+++ Desafio7/my-app/src/pages/Home.js
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
+
+import './Home.css'
+
 const Home = () => {
-  return <div>Home</div>
+  // 3- carregamento de dados
+  const url = '  http://localhost:3000/products'
+  const { data: loading, items, error } = useFetch(url)
+
+  return (
+    <div>
+      <h1>Produtos</h1>
+      {error && <p>{Error}</p>}
+      <ul className="products"></ul>
+      {items &&
+        items.map((item) => (
+          <li key={item.id}>
+            <h2>{item.name}</h2>
+            <p>R$: {item.price}</p>
+          </li>
+        ))}
+    </div>
+  )
 }
 
 export default Home
