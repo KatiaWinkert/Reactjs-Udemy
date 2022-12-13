fa3174fb0892868f2807298344ab087a33bb2d95
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-13 00:52:09 -0300

    Product.js
    Carregando dados individuais dinamicamente

--- Desafio7/my-app/src/pages/Product.js
+++ Desafio7/my-app/src/pages/Product.js
@@ -1,12 +1,26 @@
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 
 const Product = () => {
   // 4- rota dinamica
   const { id } = useParams()
 
+  // 5 carregamento dado individual
+  const url = 'http://localhost:3000/products/' + id
+
+  const { data: product, loading, error } = useFetch(url)
+
   return (
     <>
       <p>ID do produto: {id}</p>
+      {error && <p>Erro ... </p>}
+      {loading && <p>Carregando ... </p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+        </div>
+      )}
     </>
   )
 }
