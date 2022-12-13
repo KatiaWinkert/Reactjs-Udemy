454776f77a6c108ab0a2c989bcf6eb5e563bfb01
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-13 00:30:56 -0300

    Home.js
    atualização e implementação do Link to

--- Desafio7/my-app/src/pages/Home.js
+++ Desafio7/my-app/src/pages/Home.js
@@ -5,21 +5,24 @@ import './Home.css'
 
 const Home = () => {
   // 3- carregamento de dados
-  const url = '  http://localhost:3000/products'
-  const { data: loading, items, error } = useFetch(url)
+  const url = 'http://localhost:3000/products'
+  const { data: items, loading, error } = useFetch(url)
 
   return (
     <div>
       <h1>Produtos</h1>
-      {error && <p>{Error}</p>}
-      <ul className="products"></ul>
-      {items &&
-        items.map((item) => (
-          <li key={item.id}>
-            <h2>{item.name}</h2>
-            <p>R$: {item.price}</p>
-          </li>
-        ))}
+      {error && <p>{error}</p>}
+      <ul className="products">
+        {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>R$: {item.price}</p>
+              {/*4- rota dimnamica*/}
+              <Link to={`/products/${item.id}`}>Detalhes</Link>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
