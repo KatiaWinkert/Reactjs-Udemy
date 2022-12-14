b2ec3173cb26bffafff941f63917d493828c78f5
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-14 00:31:35 -0300

    feat
    inclusão de um codigo para obter um novo recurso. aula 100

--- Desafio7/my-app/src/pages/Product.js
+++ Desafio7/my-app/src/pages/Product.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
 
 const Product = () => {
@@ -19,6 +19,8 @@ const Product = () => {
         <div>
           <h1>{product.name}</h1>
           <p>{product.price}</p>
+          {/*6 - nested routes */}
+          <Link to={`/products/${product.id}/info`}>Mais informações</Link>
         </div>
       )}
     </>
