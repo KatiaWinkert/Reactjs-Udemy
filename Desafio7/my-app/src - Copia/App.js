913c7e699e18a77ab8cca00306c2f68abcac9213
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-16 10:54:29 -0300

    App.js
    implementação de modulos

---
+++ Desafio7/my-app/src - Copia/App.js
@@ -0,0 +1,47 @@
+import './App.css'
+
+// 1 - config react route
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
+import Info from './pages/Info'
+import NotFound from './pages/NotFound'
+import Search from './pages/Search'
+
+//components
+import Navbar from './Components/Navbar'
+import SearchForm from './Components/SearchForm'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Routerrrrrr</h1>
+      <BrowserRouter>
+        {/* 2 aula sobre links com react router */}
+        <Navbar />
+        {/* 9 - Search - rota de busta */}
+        <SearchForm />
+        <Routes>
+          <Route path="/" element!={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* 6 - nested route */}
+          <Route path="/products/:id/info" element={<Info />} />
+          {/*4 - rota dimanica */}
+          <Route path="/products/:id" element={<Product />} />
+          {/* 9 - search */}
+          <Route path="/search" element={<Search />} />
+            {/*10 - redirect*/}
+            <Route path="/company" element={<Navigate to="/about"/>} />
+          {/* 7 - no match route - erro 404 pag*/}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
