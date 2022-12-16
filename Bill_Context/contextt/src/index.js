87a98f0b43b0fade6f09773f3b22519649f5f40c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-16 10:49:21 -0300

    index.js
    implementação de modulos

---
+++ Bill_Context/contextt/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { CounterContextProvider } from './context/CounterContext'
+import { TitleColorContextProvider } from './context/TitleColorContext'
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
+    </CounterContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
