c6eecd960532273890d5f3774bdd71df133ab243
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-16 23:28:51 -0300

    Feat
    refatorando context

--- Bill_Context/contextt/src/page/Products.js
+++ Bill_Context/contextt/src/page/Products.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext'
 
 const Products = () => {
+  const { counter } = useContext(CounterContext)
+
   return (
-    <div>Products</div>
+    <div>
+      <h1>Produtos</h1>
+      <p>Valor do Contador: {counter} </p>
+    </div>
   )
 }
 
