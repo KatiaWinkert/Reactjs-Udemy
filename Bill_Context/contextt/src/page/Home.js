b402daa65ee436604cb236d16185030ca1dfc030
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-16 23:28:27 -0300

    perf
    melhoria de performance ( atualização )

--- Bill_Context/contextt/src/page/Home.js
+++ Bill_Context/contextt/src/page/Home.js
@@ -1,17 +1,24 @@
-import { useContext } from "react"
+//import { useContext } from 'react'
+import ChangeCounter from '../Components/ChangeCounter'
 
-import { CounterContext } from "../Context/CounterContext"
+//import { CounterContext } from '../context/CounterContext'
 
+// 4 - refatorando com hook 
+
+import { useCounterContext } from '../hooks/useCounterContex'
 
 const Home = () => {
-const {counter} = useContext(CounterContext)
+  //const { counter } = useContext(CounterContext)
+  const { counter } = useCounterContext()
 
-  return 
+  return (
     <div>
-        <h1>Home</h1>
-        <p>Valor do Contador: {counter} </p>
+      <h1>Home</h1>
+      <p>Valor do Contador: {counter} </p>
+      {/* 3 - alterando valor contexto */}
+      <ChangeCounter />
     </div>
-  
+  )
 }
 
 export default Home
