178b87f0b5c1dfed6281f2651667b9353f070680
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-16 23:26:53 -0300

    feat
    melhoria de codigos

---
+++ Bill_Context/contextt/src/Components/ChangeCounter.js
@@ -0,0 +1,17 @@
+//3- alterando contexto
+import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext'
+
+const ChangeCounter = () => {
+  const { counter, setCounter } = useContext(CounterContext)
+
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>
+        Add value to counter
+      </button>
+    </div>
+  )
+}
+
+export default ChangeCounter
