f1cff548bdd25490d4fc7b17d0493926950ccb2d
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-18 15:37:09 -0300

    perf
    Alteração de Context

--- Bill_Context/contextt/src/page/About.js
+++ Bill_Context/contextt/src/page/About.js
@@ -1,14 +1,17 @@
 //import { useContext } from 'react'
 //import { CounterContext } from '../context/CounterContext'
-import { useCounterContext } from "../hooks/useCounterContex"
+import { useCounterContext } from '../hooks/useCounterContex'
+import { useTitleColorContext } from '../hooks/useTitleColorContext'
+
 const About = () => {
   //const { counter } = useContext(CounterContext)
-const {counter} = useCounterContext()
+  const { counter } = useCounterContext()
 
+  const { color } = useTitleColorContext()
 
   return (
     <div>
-      <h1>About</h1>
+      <h1 style={{ color: color }}>About</h1>
       <p>Valor do Contador: {counter} </p>
     </div>
   )
