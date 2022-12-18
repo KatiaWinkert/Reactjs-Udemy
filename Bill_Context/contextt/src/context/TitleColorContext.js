27498de3ae53c5d3cfa5132df0f09633f9860677
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-12-18 11:27:55 -0300

    perf
    melhorando a performance do codigo

--- Bill_Context/contextt/src/context/TitleColorContext.js
+++ Bill_Context/contextt/src/context/TitleColorContext.js
@@ -1,18 +1,29 @@
-import { createContext, useReducer } from 'react'
+// contexto mais complexo
+import { createContext, useReducer, useState } from 'react'
 
 export const TitleColorContext = createContext()
 
-export const titleColorReduce = (state, action) => {
-        // switch 
-
+export const titleColorReducer = (state, action) => {
+  switch (action.type) {
+    case 'RED':
+      return { ...state, color: 'red' }
+    case 'BLUE':
+      return { ...state, color: 'blue' }
+    default:
+      return state
+  }
 }
 
-
 export const TitleColorContextProvider = ({ children }) => {
- 
- const [state, dispath] = useReducer(titleColorReduce, { color: 'purple'})
- 
-    console.log('Title color context: ', state)
-    return
- <TitleColorContext.Provider value={{...state}}>{children}</TitleColorContext.Provider>
+  const [state, dispatch] = useReducer(titleColorReducer, {
+    color: 'purple',
+  })
+
+  console.log('Title Color Context:', state)
+
+  return (
+    <TitleColorContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </TitleColorContext.Provider>
+  )
 }
